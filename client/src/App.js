565f4a165a53cd780f71c833f3a0912f2534654e
Ignacio <ignacio.puglisi@gmail.com>
2021-01-31 00:52:18 +0000

    useState

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,28 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [foodName, setFoodName] = useState('');
+  const [days, setDays] = useState(0);
+
+  const addToList = () => {
+    console.log(foodName, days);
+  }
+
   return (
     <div className="App">
       <h1>CRUD App with MERN</h1>
 
       <label htmlFor="">Food Name:</label>
-      <input type="text" />
+      <input type="text"
+        onChange={(event) => { setFoodName(event.target.value) }}
+      />
       <label htmlFor="">Days Since You Ate It:</label>
-      <input type="number" />
-      <button>Add to List</button>
+      <input type="number"
+        onChange={(event) => { setDays(event.target.value) }}
+      />
+      <button onClick={addToList}>Add to List</button>
     </div>
   );
 }
