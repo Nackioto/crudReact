a11502386ac9e399fd2e73e182871916bcd0b5ce
Ignacio <ignacio.puglisi@gmail.com>
2021-01-31 01:33:23 +0000

    cors

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
 const FoodModel = require('./models/Food');
 
 app.use(express.json());
+app.use(cors());
 
-mongoose.connect('mongodb+srv://newUser:Glasgow1234@cluster0.opued.mongodb.net/food?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-});
+mongoose.connect('mongodb+srv://newUser:Glasgow1234@cluster0.opued.mongodb.net/food?retryWrites=true&w=majority',
+    { useNewUrlParser: true, });
 
 app.post('/insert', async (req, res) => {
+
     const foodName = req.body.foodName
     const days = req.body.days
+
     const food = new FoodModel({ foodName: foodName, daysSinceIAte: days });
 
     try {
